3e08ed252bc39ae50cf52c4b6b7844a7d0beb5ce
Tom Rives <tom.rives@epitech.eu>
2024-07-20 16:12:36 +0200

    [INIT] GST Website

---
+++ src/app/page.tsx
@@ -0,0 +1,80 @@
+import { Header } from "@/components/header";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className="flex min-h-screen flex-col">
+        <section className="p-10" style={{ backgroundColor: "#1E1F1C" }}>
+          <div className="mx-auto max-w-7xl px-6 lg:px-8 text-center">
+            <div className="mx-auto max-w-2xl lg:text-center">
+              <div className="flex justify-center item-center">
+                <Image
+                  src="/gst_icon.png"
+                  alt="GST Icon"
+                  width={96}
+                  height={96}
+                />
+              </div>
+              <h2 className="md:text-5xl text-3xl font-bold p-2">
+                Game Server Tracker API
+              </h2>
+              <h2 className="text-base font-semibold leading-7 text-primary">
+                A simplest way to track your game server
+              </h2>
+              <p className="mt-6 text-lg leading-8">
+                GST gives to you information like number of players connected on
+                various game servers (Minecraft, Source, GTA V).
+              </p>
+            </div>
+          </div>
+        </section>
+        <div className="container min-h-screen pt-6 lg:pt-10">
+          <div className=" flex justify-center items-center">
+            <Card className="w-[380px]">
+              <CardHeader>
+                <CardTitle>Track a server</CardTitle>
+                <CardDescription>
+                  Provide the server address and get the information
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <div className="space-y-3">
+                  <Input value="mc.hypixel.net" placeholder="Server address" />
+                  <Select>
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Theme" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="light">Light</SelectItem>
+                      <SelectItem value="dark">Dark</SelectItem>
+                      <SelectItem value="system">System</SelectItem>
+                    </SelectContent>
+                  </Select>
+                  <Button>Track it</Button>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { CodeIcon } from "@radix-ui/react-icons";
+
+export function Header() {
+  return (
+    <div className="flex items-center justify-between px-2">
+      <div className="flex flex-row gap-3 p-5">
+        <CodeIcon width={32} height={32} />
+        <h1 className="font-heading text-xl md:text-2xl font-bold">Game Server Tracker</h1>
+      </div>
+      <div>
+        <Link href="#">
+          <Button variant="link">Swagger</Button>
+        </Link>
+        <Link href="#">
+          <Button variant="link">Contact</Button>
+        </Link>
+      </div>
+    </div>
+  );
+}
