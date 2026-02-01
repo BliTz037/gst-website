e2eaafd7741027fb865f21fd5379c0b2f8fdab7f
Tom Rives <blitz@blitzlab.ninja>
2026-02-01 15:01:22 +0100

    Update ServerTypeCard.tsx

--- src/components/cards/ServerTypeCard.tsx
+++ src/components/cards/ServerTypeCard.tsx
@@ -3,22 +3,25 @@ import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { MinecraftIcon } from "../icons/MinecraftIcon";
 import { SourceIcon } from "../icons/SourceIcon";
 import { FiveMIcon } from "../icons/FiveMIcon";
+import React from "react";
 
-const serverTypeIcons: { [key: string]: () => JSX.Element } = {
-  'mc': () => <MinecraftIcon width={64} height={64} />,
-  'mcbedrock': () => <Image src="/bedrock.webp" alt="Bedrock" width={64} height={64} />,
-  'source': () => <SourceIcon width={64} height={64} color="#f0db4f" />,
-  'fivem': () => <FiveMIcon width={64} height={64} color="white" />,
+const serverTypeIcons: { [key: string]: () => React.JSX.Element } = {
+  mc: () => <MinecraftIcon width={64} height={64} />,
+  mcbedrock: () => (
+    <Image src="/bedrock.webp" alt="Bedrock" width={64} height={64} />
+  ),
+  source: () => <SourceIcon width={64} height={64} color="#f0db4f" />,
+  fivem: () => <FiveMIcon width={64} height={64} color="white" />,
 };
 
 const serverTypeName: { [key: string]: () => string } = {
-  'mc': () => "Minecraft Java Edition",
-  'mcbedrock': () => "Minecraft Bedrock Edition",
-  'source': () => "Source",
-  'fivem': () => "FiveM / RedM",
+  mc: () => "Minecraft Java Edition",
+  mcbedrock: () => "Minecraft Bedrock Edition",
+  source: () => "Source",
+  fivem: () => "FiveM / RedM",
 };
 
-export const ServerTypeCard = ({serverType}: {serverType: string}) => {
+export const ServerTypeCard = ({ serverType }: { serverType: string }) => {
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
